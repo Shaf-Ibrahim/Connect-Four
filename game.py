cb4559dd24b1b9f6c730d857fa15ce1681c47e46
Shaf Ibrahim <smi32@cornell.edu>
2020-07-01 14:47:04 -0700

    Variable refactor

--- game.py
+++ game.py
@@ -6,6 +6,8 @@ import config
 
 board = []
 player_turn = 1
+running = True
+winner = 0
 
 pygame.init()
 
@@ -49,13 +51,15 @@ def is_valid_location(move):
     global board
     return board[0][move] == 0
 
-def place_piece(player_turn, move):
-    row = _get_row(player_turn, move)
-    draw_piece(player_turn, row, move)
+def place_piece(move):
+    global player_turn
+    row = _get_row(move)
+    draw_piece(row, move)
     return
 
-def _get_row(player_turn, move):
+def _get_row(move):
     global board
+    global player_turn
     row = 0
     while row < config.BOARD_ROWS and board[row][move] == 0:
         row += 1
@@ -63,30 +67,34 @@ def _get_row(player_turn, move):
     board[row][move] = player_turn
     return row
 
-def draw_piece(player_turn, row, col):
+def draw_piece(row, col):
+    global player_turn
     color = config.RED if player_turn == 1 else config.YELLOW
     pygame.draw.circle(screen, color,
                       (int(col * config.RECT_L + config.RECT_L/2), int(row * config.RECT_L + config.RECT_L + config.RECT_L/2)),
                       config.RADIUS)
 
-def drop_animation(start_pos, end_pos, player_turn):
+def drop_animation(start_pos, end_pos):
+    global player_turn
     pass
 
-def check_win(player_turn):
+def check_win():
     global board
+    global player_turn
     for row in range(config.BOARD_ROWS):
         for col in range(config.BOARD_COLS):
             if board[row][col] == player_turn:
-                horWin = _check_horizontal(player_turn, row)
-                verWin = _check_vertical(player_turn, col)
-                posDiagWin = _check_pos_diagonal(player_turn, row, col)
-                negDiagWin = _check_neg_diagonal(player_turn, row, col)
+                horWin = _check_horizontal(row)
+                verWin = _check_vertical(col)
+                posDiagWin = _check_pos_diagonal(row, col)
+                negDiagWin = _check_neg_diagonal(row, col)
                 if horWin or verWin or posDiagWin or negDiagWin:
                     return True
     return False
 
-def _check_horizontal(player_turn, row):
+def _check_horizontal(row):
     global board
+    global player_turn
     piece_count = 0
     for piece in board[row]:
         if piece == player_turn:
@@ -98,8 +106,9 @@ def _check_horizontal(player_turn, row):
                 piece_count = 0
     return False
 
-def _check_vertical(player_turn, col):
+def _check_vertical(col):
     global board
+    global player_turn
     piece_count = 0
     for row in board:
         if row[col] == player_turn:
@@ -111,8 +120,9 @@ def _check_vertical(player_turn, col):
                 piece_count = 0
     return False
 
-def _check_pos_diagonal(player_turn, row, col):
+def _check_pos_diagonal(row, col):
     global board
+    global player_turn
     rowIter = row
     colIter = col
     count = 0
@@ -138,8 +148,9 @@ def _check_pos_diagonal(player_turn, row, col):
         colIter -= 1
     return False
 
-def _check_neg_diagonal(player_turn, row, col):
+def _check_neg_diagonal(row, col):
     global board
+    global player_turn
     rowIter = row
     colIter = col
     count = 0
@@ -169,7 +180,8 @@ def init():
     init_board()
     create_board()
 
-def hover_circle(mouse_pos, player_turn):
+def hover_circle(mouse_pos):
+    global player_turn
     pygame.draw.rect(screen, config.BLACK,
                     (0, 0, config.BOARD_COLS * config.RECT_L, config.RECT_H))
     display.update()
@@ -180,28 +192,38 @@ def hover_circle(mouse_pos, player_turn):
     pygame.draw.circle(screen, color, center_pos, config.RADIUS)
     display.update()
 
+def error_message():
+    clock = pygame.time.Clock()
+    font = pygame.font.Font(None, 64)
+    blue = pygame.Color('royalblue')
+    orig_surf = font.render('Enter your text', True, blue)
+    txt_surf = orig_surf.copy()
+    # This surface is used to adjust the alpha of the txt_surf.
+    alpha_surf = pygame.Surface(txt_surf.get_size(), pygame.SRCALPHA)
+    alpha = 255  # The current alpha value of the surface.
+
+    if alpha > 0:
+        # Reduce alpha each frame, but make sure it doesn't get below 0.
+        alpha = max(alpha-4, 0)
+        txt_surf = orig_surf.copy()  # Don't modify the original text surf.
+        # Fill alpha_surf with this color to set its alpha value.
+        alpha_surf.fill((255, 255, 255, alpha))
+        # To make the text surface transparent, blit the transparent
+        # alpha_surf onto it with the BLEND_RGBA_MULT flag.
+        txt_surf.blit(alpha_surf, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
+
+    screen.fill((30, 30, 30))
+    screen.blit(txt_surf, (30, 60))
+    pygame.display.flip()
+    clock.tick(30)
 
 init()
-
-running = True
-winner = 0
-
-# while running:
-#     move = get_player_move(player_turn)
-#     while move is None:
-#         move = get_player_move(player_turn)
-#     place_piece(player_turn, move)
-#     if check_win(player_turn):
-#         winner = player_turn
-#         break
-#     print_board()
-#     player_turn = 1 if player_turn == 2 else 2
 
 # GUI
 while running:
     events = event.get()
 
-    hover_circle(pygame.mouse.get_pos(), player_turn)
+    hover_circle(pygame.mouse.get_pos())
 
     for e in events:
         if e.type == pygame.QUIT:
@@ -214,11 +236,13 @@ while running:
         if e.type == pygame.MOUSEBUTTONDOWN:
             move = get_player_move(pygame.mouse.get_pos())
             if is_valid_location(move):
-                place_piece(player_turn, move)
+                place_piece(move)
+                text = font.render('Piece placed', True, config.RED)
             else:
-                error_message(player_turn, move)
-            if check_win(player_turn):
+                while not is_valid_location(move):
+                    error_message()
+                    move = get_player_move(pygame.mouse.get_pos())
+            if check_win():
                 winner = player_turn
-                screen.fill((255, 255, 255))
-                break
+                running = False
             player_turn = 1 if player_turn == 2 else 2
